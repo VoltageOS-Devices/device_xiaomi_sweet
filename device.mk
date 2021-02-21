0bc9e9bce90ae5cef2759add3bd8496468a5379e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-21 16:46:06 +0100

    sweet: Build android.hidl.base@1.0
    Change-Id: I5457ebbadd95e4c43af4f4dd2e1a1413a241d865

--- device.mk
+++ device.mk
@@ -177,6 +177,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom
     
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0.vendor
+
 # Init scripts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.sweet.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.sweet.rc
