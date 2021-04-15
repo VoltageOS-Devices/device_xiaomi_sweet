b73ad302998d40890560cfb0c0d389d50489fa2b
adarshgrewal <adarshgrewal@gmail.com>
2021-04-15 15:22:52 +0530

    sweet: import stock qdcm profile
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- device.mk
+++ device.mk
@@ -170,6 +170,10 @@ PRODUCT_PACKAGES += \
     FM2 \
     qcom.fmradio
 
+# Display
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/display/qdcm_calib_data_xiaomi_k6_38_0c_0a_fhd_dsc_video_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_xiaomi_k6_38_0c_0a_fhd_dsc_video_dsi_panel.xml
+
 # Fstab
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.zram:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.zram \
