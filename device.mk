1d9422badca00cebcb13a1aef007fa026565bcf8
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-04-26 11:00:12 +0530

    sweet: Add KeyHandler for side-mounted fingerprint key
    * This prevents the fingerprint sensor from triggering other events.
    
    Change-Id: I2b4543f2f02f974d52e96739569ba4aa9697c77e
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- device.mk
+++ device.mk
@@ -97,6 +97,10 @@ PRODUCT_PACKAGES += \
     init.qti.dcvs.sh \
     init.qti.qcv.sh
 
+# Keyhandler
+PRODUCT_PACKAGES += \
+    KeyHandler
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl \
