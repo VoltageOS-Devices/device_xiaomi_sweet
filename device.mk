fb7feb9a909cc4b7758f34aaf88e18a9ddd8ddc3
Zinadin Zidan <zidan.roking@gmail.com>
2021-07-23 16:53:20 +0600

    sweet: wifi: Import wifi configs
    * From sweet-user 11 RKQ1.200826.002 V12.5.4.0.RKFMIXM release-keys
    
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I5c9dd48927c5df4ae1630d6f1bcc0402df49c46a

--- device.mk
+++ device.mk
@@ -313,6 +313,11 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.wifi.hostapd@1.2.vendor \
     vendor.qti.hardware.wifi.supplicant@2.2.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
