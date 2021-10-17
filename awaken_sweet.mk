a287eb889289ff8e74160f28cca625250ea7d506
Niranjan BR <niranjankannan2003@gmail.com>
2021-10-18 00:46:31 +0530

    sweet: Build with GApps
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- awaken_sweet.mk
+++ awaken_sweet.mk
@@ -24,3 +24,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys"
 
 BUILD_FINGERPRINT := Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys
+
+# GApps
+USE_GAPPS := true
+WITH_GAPPS := true
