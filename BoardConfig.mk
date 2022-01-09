d6d176f76bd7bbf6db7b80ba5482ecf8994551e0
Niranjan BR <niranjankannan2003@gmail.com>
2022-01-09 12:32:29 +0530

    sweet: Vantomized *switched to Vantom kernel & compile with proton clang
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,7 +45,8 @@ BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CONFIG := sweet_user_defconfig
+TARGET_KERNEL_CLANG_VERSION := proton
+TARGET_KERNEL_CONFIG := sweet_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sweet
 TARGET_KERNEL_ARCH := arm64
 
