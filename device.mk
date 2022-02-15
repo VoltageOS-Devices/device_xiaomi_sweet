669645985397fa86096b6530191dcd141314c8f3
Robert Shih <robertshih@google.com>
2022-02-14 16:02:55 -0800

    sweet: Enable AIDL DRM HALs
    Bug: 219538389
    Test: atest GtsMediaTestCases
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- device.mk
+++ device.mk
@@ -165,7 +165,7 @@ TARGET_EXCLUDES_AUDIOFX := true
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm-service.clearkey \
     android.hardware.drm@1.4.vendor
 
 # Define board
