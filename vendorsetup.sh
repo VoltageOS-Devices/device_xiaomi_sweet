ad6400cdfd8b6797a38eb5ccc7ebe9da88773d39
Niranjan BR <niranjankannan2003@gmail.com>
2022-03-17 10:59:00 +0000

    sweet: include vendorsetup.sh - Track Hals - Track Vendor - Track Kernel & Proton clang - Track pixel additional stuffs
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,20 @@
+# Nuke 
+rm -rf hardware/qcom-caf/sm8150/display 
+rm -rf hardware/qcom-caf/sm8150/audio
+rm -rf hardware/qcom-caf/sm8150/media
+
+# Hals
+git clone https://github.com/ArrowOS/android_hardware_qcom_audio -b arrow-12.0-caf-sm8150 hardware/qcom-caf/sm8150/audio 
+git clone https://github.com/ArrowOS/android_hardware_qcom_media -b arrow-12.0-caf-sm8150 hardware/qcom-caf/sm8150/media 
+git clone https://github.com/ArrowOS/android_hardware_qcom_display -b arrow-12.0-caf-sm8150 hardware/qcom-caf/sm8150/display
+
+# Vendor
+git clone https://github.com/mrfox2003/vendor_xiaomi_sweet.git -b 12 vendor/xiaomi/sweet
+
+# Kernel & Clang
+git clone -b courbet --depth=1 https://github.com/vantoman/kernel_xiaomi_sm6150.git kernel/xiaomi/sweet
+git clone -b master --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
+
+# Additional Pixel stuffs
+git clone https://github.com/mrfox2003/vendor-pixel-additional.git -b twelve vendor/pixel-additional 
+
