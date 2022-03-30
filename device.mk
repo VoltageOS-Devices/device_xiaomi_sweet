b2ec7b19eaee1df8dcf7e804130cc985d8c0c53d
johnmart19 <johnivan19999@gmail.com>
2022-03-30 19:48:25 +0300

    sweet: Build missing bluetooth libs
    E a2dp_offload: open_a2dp_sink: DLOPEN failed for libbthost_if_sink.so
    
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- device.mk
+++ device.mk
@@ -98,6 +98,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
+    libbthost_if \
+    libbthost_if.vendor \
+    libldacBT_bco.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
