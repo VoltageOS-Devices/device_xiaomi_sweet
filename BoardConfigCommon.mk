9c75c1197cce7bf0f0cd04828d8ce271867d8570
dev-harsh1998 <god@hyper-labs.tech>
2022-05-07 14:27:32 +0800

    sm6150-common: Compile HWUI for performance
    * Activates few optimizing compiler flags for HWUI.
    * -fno-omit-frame-pointer, -marm, -mapcs
    
    Change-Id: Ia1391b1a6cbfd876459042f88e58f2eafa17d9d6
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>
    Signed-off-by: Pascoato <viniciuspascoato@gmail.com>
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -79,6 +79,9 @@ DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/hidl/manifest.xml
 DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/hidl/compatibility_matrix.xml
 
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
