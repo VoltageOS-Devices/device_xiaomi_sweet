654e8694d90bf71c477d69304e152428ee82697a
Niranjan BR <niranjankannan2003@gmail.com>
2022-06-14 00:34:37 +0000

    Welcome Cherish OS
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -277,7 +277,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-evolution
+    $(LOCAL_PATH)/overlay-cherish
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
