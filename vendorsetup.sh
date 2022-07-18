bc5e6c16306deb3112860cff77efb6e6f1d42755
Niranjan BR <niranjankannan2003@gmail.com>
2022-07-18 11:02:08 +0000

    sweet: vendorsetup.sh: Track Vendor from org
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,7 +9,7 @@ git clone https://github.com/ArrowOS/android_hardware_qcom_media -b arrow-12.0-c
 git clone https://github.com/ArrowOS/android_hardware_qcom_display -b arrow-12.0-caf-sm8150 hardware/qcom-caf/sm8150/display
 
 # Vendor
-git clone https://github.com/mrfox2003/vendor_xiaomi_sweet.git -b 12 vendor/xiaomi/sweet
+git clone https://github.com/VoltageOS-Devices/vendor_xiaomi_sweet -b 12 vendor/xiaomi/sweet
 
 # Kernel & Clang
 git clone -b courbet-12.1 --depth=1 https://github.com/vantoman/kernel_xiaomi_sm6150.git kernel/xiaomi/sweet
