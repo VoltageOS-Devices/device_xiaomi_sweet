b1aff7b90781bdb843fc8a3f29a56b53251a4469
daniml3 <daniel@danielml.dev>
2022-08-19 13:56:19 +0200

    sweet: Use GetPreviewImageData shim for com.qti.node.watermark
    Signed-off-by: daniml3 <daniel@danielml.dev>
    Change-Id: I92e51fdf1dc7d877c7affafbf9803ba5d504be07

--- device.mk
+++ device.mk
@@ -170,6 +170,10 @@ PRODUCT_PACKAGES += \
     FM2 \
     qcom.fmradio
 
+# Camera
+PRODUCT_PACKAGES += \
+    libpiex_shim
+
 # Display
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/qdcm_calib_data_xiaomi_k6_38_0c_0a_fhd_dsc_video_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_xiaomi_k6_38_0c_0a_fhd_dsc_video_dsi_panel.xml
