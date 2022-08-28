9981d5f4003014a85ec0275c777cc6cc22f83831
Niranjan BR <niranjankannan2003@gmail.com>
2022-08-28 04:29:56 +0000

    sweet:Include vendorsetup.sh
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,16 @@
+# Nuke 
+rm -rf hardware/qcom-caf/sm8150/display 
+rm -rf hardware/qcom-caf/sm8150/audio
+rm -rf hardware/qcom-caf/sm8150/media
+
+# Hals
+git clone https://github.com/ArrowOS/android_hardware_qcom_audio -b arrow-13.0-caf-sm8150 hardware/qcom-caf/sm8150/audio 
+git clone https://github.com/ArrowOS/android_hardware_qcom_media -b arrow-13.0-caf-sm8150 hardware/qcom-caf/sm8150/media 
+git clone https://github.com/ArrowOS/android_hardware_qcom_display -b arrow-13.0-caf-sm8150 hardware/qcom-caf/sm8150/display
+
+# Vendor
+git clone https://github.com/VoltageOS-Devices/vendor_xiaomi_sweet -b 13.0 vendor/xiaomi/sweet
+
+# Kernel & Clang
+git clone -b courbet-12.1 --depth=1 https://github.com/vantoman/kernel_xiaomi_sm6150.git kernel/xiaomi/sweet
+git clone -b 15 --depth=1 https://gitlab.com/PixelOS-Devices/playgroundtc.git prebuilts/clang/host/linux-x86/clang-playground
