1d551710a8426fc4ac3ed6ac35cfc2710fc6b843
Niranjan BR <niranjankannan2003@gmail.com>
2022-09-10 07:37:12 +0000

    sm6150-common: Welcome Voltage OS
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -298,7 +298,7 @@ PRODUCT_PACKAGES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-cherish
+    $(LOCAL_PATH)/overlay-voltage
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
