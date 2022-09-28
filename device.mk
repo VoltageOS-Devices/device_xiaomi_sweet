8ab8f8736be5b0f795ccf8b9caff5e1d0874e0f5
sureshfizzy <sureshfizzy0503@gmail.com>
2022-09-28 12:35:40 +0530

    sweet: Import OnePlus Dolby
    Co-authored-by: Anushek Prasal <anushekprasal@gmail.com>
    Co-authored-by: HELLBOY017 <abhaygill017@gmail.com>
    Co-authored-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Co-authored-by: ziasam <ziauddinsameer@gmail.com>
    Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>
    Co-authored-by: sureshfizzy <sureshfizzy0503@gmail.com>
    Co-authored-by: Niranjan BR <niranjankannan2003@gmail.com>

--- device.mk
+++ device.mk
@@ -167,7 +167,17 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi_sm6150
-    
+
+# Dolby
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_ODM)/etc/dolby/dax-default.xml \
+    $(LOCAL_PATH)/configs/dolby/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml \
+    $(LOCAL_PATH)/configs/dolby/config-com.dolby.daxappui.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/config-com.dolby.daxappui.xml \
+    $(LOCAL_PATH)/configs/dolby/config-com.dolby.daxservice.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/config-com.dolby.daxservice.xml \
+    $(LOCAL_PATH)/configs/dolby/hiddenapi-com.dolby.daxservice.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/hiddenapi-com.dolby.daxservice.xml \
+    $(LOCAL_PATH)/configs/dolby/privapp-com.dolby.daxappui.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-com.dolby.daxappui.xml \
+    $(LOCAL_PATH)/configs/dolby/privapp-com.dolby.daxservice.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-com.dolby.daxservice.xml
+
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
