b31c71d3ff9d0b307379b474943798fd79154788
Niranjan BR <niranjankannan2003@gmail.com>
2022-10-02 23:17:29 +0530

    sweet: Set default USB and Bluetooth name

--- init/init_sweet.cpp
+++ init/init_sweet.cpp
@@ -84,12 +84,14 @@ void vendor_load_properties() {
     std::string fingerprint;
     std::string description;
     std::string mod_device;
+    std::string marketname;
 
     if (region == "GLOBAL") {
         model = "M2101K6G";
         device = "sweet";
         fingerprint = "Redmi/sweet_eea/sweet:13/RKQ1.210614.002/V14.0.2.0.TKFEUXM:user/release-keys";
         description = "sweet_eea-user 13 RKQ1.210614.002 V14.0.2.0.TKFEUXM release-keys";
+        marketname = "Redmi Note 10 Pro";
         mod_device = "sweet_eea_global";
         property_override("ro.boot.product.hardware.sku", "sweet");
     } else if (region == "JAPAN") {
@@ -97,6 +99,7 @@ void vendor_load_properties() {
         device = "sweet";
         fingerprint = "Redmi/sweet_global/sweet:13/RKQ1.210614.002/V14.0.1.0.TKFMIXM:user/release-keys";
         description = "sweet_global-user 13 RKQ1.210614.002 V14.0.1.0.TKFMIXM release-keys";
+        marketname = "Redmi Note 10 Pro";
         mod_device = "sweet_global";
         property_override("ro.boot.product.hardware.sku", "sweet");
     } else if (region == "INDIA") {
@@ -105,12 +108,14 @@ void vendor_load_properties() {
             device = "sweetin";
             fingerprint = "Redmi/sweetin/sweetin:13/RKQ1.210614.002/V14.0.1.0.TKFINXM:user/release-keys";
             description = "sweetin-user 13 RKQ1.210614.002 V14.0.1.0.TKFINXM release-keys";
+            marketname = "Redmi Note 10 Pro";
             mod_device = "sweetin_in_global";
         } else {
             model = "M2101K6I";
             device = "sweetin";
             fingerprint = "Redmi/sweetinpro/sweetin:13/RKQ1.210614.002/V14.0.1.0.TKFINXM:user/release-keys";
             description = "sweetinpro-user 13 RKQ1.210614.002 V14.0.1.0.TKFINXM release-keys";
+            marketname = "Redmi Note 10 Pro Max";
             mod_device = "sweetin_in_global";
             }
     }
@@ -122,4 +127,6 @@ void vendor_load_properties() {
     if (mod_device != "") {
         property_override("ro.product.mod_device", mod_device.c_str());
     }
+    property_override("bluetooth.device.default_name", marketname.c_str());
+    property_override("vendor.usb.product_string", marketname.c_str());
 }
