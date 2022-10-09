b44127a426d50ae5447eb2ea573092ac0bd89133
Kuba Wojciechowski <nullbytepl@gmail.com>
2022-10-09 06:56:22 +0000

    sm6150-common: cmdline: Disable kpti * It can also help increase performance by a lot in some scenarios.
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -99,6 +99,7 @@ BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 KERNEL_LD := LD=ld.lld
+BOARD_KERNEL_CMDLINE += kpti=off
 
 # TARGET_KERNEL_APPEND_DTB handling
 ifeq ($(strip $(PRODUCT_USE_DYNAMIC_PARTITIONS)),true)
