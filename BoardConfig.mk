fb7ca692da0f695144568476cc37c26b45518e81
Niranjan BR <niranjankannan2003@gmail.com>
2023-05-25 23:12:29 +0000

    sweet: Convert xiaomi_framework_compatibility_matrix.xml into device_framework_matrix.xml * also clean up
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,7 +99,7 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    $(DEVICE_PATH)/configs/hidl/xiaomi_framework_compatibility_matrix.xml \
+    $(DEVICE_PATH)/configs/hidl/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 
 ODM_MANIFEST_SKUS += sweet
