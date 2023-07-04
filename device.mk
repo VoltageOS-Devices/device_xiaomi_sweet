f7c7d47dfcb40e3423c863ca2a97448bcc392f69
Yung Ti Su <andysu@google.com>
2023-07-04 04:36:44 +0000

    sweet: Enabling sound trigger V2_3 for S5 Bug: 139071862 Test: manual
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- device.mk
+++ device.mk
@@ -44,7 +44,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio-impl
+    android.hardware.bluetooth.audio-impl \
+    android.hardware.soundtrigger@2.3-impl
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
