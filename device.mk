0630026efda87905aec03295d717a628cf980d72
Josh Wu <joshwu@google.com>
2023-12-07 08:06:16 +0000

    sweet: Switch BtAudio to AIDL

--- device.mk
+++ device.mk
@@ -50,8 +50,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
-    android.hardware.soundtrigger@2.3-impl \
-    android.hardware.bluetooth.audio@2.1-impl
+    android.hardware.bluetooth.audio-impl \
+    android.hardware.soundtrigger@2.3-impl
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
