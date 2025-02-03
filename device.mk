98e8a998076c1923ac431d9f406d01458a5cbc96
basamaryan <basam.aryan@gmail.com>
2025-02-03 10:31:04 -0500

    sweet: media: Import media_codecs.xml
    From https://github.com/LineageOS/android_device_google_sunfish/blob/b2d81d3d106ef961ee1ebd036dbbb5d1b0013807/media_codecs.xml
    
    Change-Id: Ia3905329085e72a55c9e964e07ecd8a000a913f0

--- device.mk
+++ device.mk
@@ -299,6 +299,7 @@ PRODUCT_PACKAGES += \
     libcodec2_hidl@1.0.vendor 
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor.xml \
