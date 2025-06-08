ccd86db5eab65e4d5d7a5f0a3943596f9e71e116
Michael Bestas <mkbestas@lineageos.org>
2025-06-08 06:36:48 +0000

    sweet: Disable OMX service
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- voltage_sweet.mk
+++ voltage_sweet.mk
@@ -7,6 +7,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+TARGET_SUPPORTS_OMX_SERVICE := false
 
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
