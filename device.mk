ad8f568bc79ab0516cb2f240cbda4615ae9fa6e7
Niranjan BR <niranjankannan2003@gmail.com>
2025-07-07 05:25:46 +0000

    sweet: Build android.hardware.audio.sounddose-vendor-impl * audiohalservice: Failed to dlopen android.hardware.audio.sounddose-vendor-impl.so: dlopen failed: library "android.hardware.audio.sounddose-vendor-impl.so" not found
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- device.mk
+++ device.mk
@@ -55,7 +55,8 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl\
     libaudioroute.vendor\
     libstagefright_softomx_plugin.vendor\
-    android.hardware.soundtrigger@2.3-impl
+    android.hardware.soundtrigger@2.3-impl\
+    android.hardware.audio.sounddose-vendor-impl
 
 
 PRODUCT_PACKAGES += \
