1459eeacf831387e7af0ff9f7bc2c51610d17bcf
Niranjan BR <niranjankannan2003@gmail.com>
2025-07-11 07:02:57 +0000

    sweet: inlcude dolby_manifest.xml *sync with dolby repo
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -95,6 +95,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
 
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
+DEVICE_MANIFEST_FILE += vendor/oneplus/dolby/vintf/dolby_manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
 # HWUI
