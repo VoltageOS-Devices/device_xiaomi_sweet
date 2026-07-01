a2ecd0231b8d5761a312221c3167154cee488cc7
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-01 01:33:26 -0400

    sweet: Use legacy libion implementation
    * At least one blob directly depends on this, with
      many other bits and pieces likely.
    
    Change-Id: Ic063618d6942a4f6e90dd7e21698e36d14113d06
    Signed-off-by: Frost <frost.github@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -143,6 +143,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmsteppe
+include device/voltage/sepolicy/libion/sepolicy.mk
 include device/voltage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
